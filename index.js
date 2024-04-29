65a7c61c084b583a84227399ec9481edbe395316
eduardomagaldi <magaldi1989@gmail.com>
2024-04-29 08:51:58 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("inicio");
+// tipo numero
+var numero = 3;
+var numero2 = 7;
+console.log(numero + numero2);
+console.log(numero - numero2);
+console.log(numero / numero2);
+console.log(numero * numero2);
+
+// tipo boolean
+var boolean1 = true;
+var boolean2 = false;
+console.log(true && true && true);
+console.log(false || false || false);
