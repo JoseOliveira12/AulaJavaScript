72ee127550b81cee1106ad874424c6888337a290
eduardomagaldi <magaldi1989@gmail.com>
2024-05-09 09:58:41 -0300

    Create if.js

---
+++ if.js
@@ -0,0 +1,16 @@
+var homem = true
+var trabalhador = true
+var mulher = false
+var temFilhos = 'Sim'
+
+if (mulher) {
+  console.log('pode engravidar', !mulher)
+}
+
+if (mulher == false) {
+  console.log('nao pode engravidar.', !mulher)
+}
+
+if (temFilhos == 'Sim') {
+  console.log('tem filhos.')
+}
